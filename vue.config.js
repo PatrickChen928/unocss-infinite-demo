12a4e200ca4fcb5ad5416c6743372ef71b5355bc
cyly <786156072@qq.com>
2022-08-17 16:24:45 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const UnoCSS = require('@unocss/webpack').default
+const { transformerVariantGroup } = require('unocss')
+
+module.exports = {
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      UnoCSS({
+        transformers: [
+          transformerVariantGroup(),
+        ],
+      }),
+    ],
+  },
+  chainWebpack(config) {
+    config.module.rule('vue').uses.delete('cache-loader')
+    config.module.rule('tsx').uses.delete('cache-loader')
+    config.merge({
+      cache: false,
+    })
+  },
+}
